b920b197b50d9913573fb6863fdfc110cae89a82
JATIN SHARMA <117714251+vashistJatin@users.noreply.github.com>
2025-05-15 12:24:51 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -34,7 +34,7 @@ export default function RootLayout({ children }) {
           </div>
 
           {/* Main content scrollable */}
-          <div className="flex-1 xl:overflow-y-hidden overflow-y-auto">
+          <div className="flex-1 xl:overflow-hidden overflow-y-auto">
             <div className="max-w-[1130px] px-[15px] py-6 mx-auto">
               <Header className="mb-2" />
               <div>{children}</div>
